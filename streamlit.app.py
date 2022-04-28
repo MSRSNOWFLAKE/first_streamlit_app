040077f3f8ac0dbfc64e1ff44d211e6ae9db171c
MSRSNOWFLAKE <104140448+MSRSNOWFLAKE@users.noreply.github.com>
2022-04-28 12:56:35 +0200

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -16,3 +16,5 @@ fruits_selected= streamlit.multiselect ("Pick some fruits: ", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc [fruits_selected]
 #display table
 streamlit.dataframe(fruits_to_show)
+
+import requests
